a287e9cacede7b6ecabab841bbbd2c5fb2fb5f64
Leonardo Contreras <levt.contreras@proton.me>
2025-03-19 13:36:09 -0600

    Added charist to package.json and changed the index html to a centered chart.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import {Chart} from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+
+  ngAfterViewInit(){
+
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    new Chart('acquisitions', {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count),
+            backgroundColor: 'rgba(235, 54, 220, 0.75)', // Optional styling
+            
+          }
+        ]
+      }
+    });  
+  }
+    
 }
