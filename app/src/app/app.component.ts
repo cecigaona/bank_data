b1c9c05d0ff3ef7e8eaf64ac5b8cda4372a4277d
Leonardo Contreras <levt.contreras@proton.me>
2025-04-22 16:00:10 -0600

    Added pie chart which uses the dataset, installed chart-js-datalabels plugin (npm install required)

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,25 +1,64 @@
 import { AfterViewInit, Component } from '@angular/core';
-import {Chart} from 'chart.js/auto';
+import { Chart } from 'chart.js/auto';
 import { SqlService } from './sql.service';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+Chart.register(ChartDataLabels);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'app';
 
-  constructor(private sql:SqlService){
+  constructor(private sql: SqlService) {
 
   }
 
-  
+  ngAfterViewInit() {
+    const query = 'SELECT Credit_Score, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM transactions) AS percentage FROM transactions GROUP BY Credit_Score;'
 
-  ngAfterViewInit(){
-    this.sql.query('SELECT * FROM test').subscribe({
+    let data_credit: any[] = []
+    this.sql.query(query).subscribe({
       next: (response) => {
         console.log('Response data:', response);
+        data_credit = response
+
+        const data = {
+          labels: data_credit.map(row => row.Credit_Score),
+          datasets: [{
+            data: data_credit.map(row => row.percentage),
+            backgroundColor: ['rgba(110, 217, 221, 0.75)', 'rgba(110, 195, 221, 0.75)', 'rgba(110, 177, 221, 0.75)'],
+          }]
+        };
+
+        new Chart('credit_score', {
+          type: 'pie',
+          data: data,
+          options: {
+            plugins: {
+              datalabels: {
+                formatter: (value:number, context:any) => {
+                  const dataset = context.chart.data.datasets[0].data;
+                  const total = dataset.reduce((a:number, b:number) => a + b, 0);
+                  const percentage = ((value / total) * 100).toFixed(1);
+                  return percentage + '%';
+                },
+                color: '#000',
+                anchor: 'end',
+                align: 'start',
+                offset: 30,
+                font: {
+                  weight: 'bold',
+                  size: 24,
+                }
+              }
+            }
+          },
+          plugins: [ChartDataLabels]
+        });
       },
       error: (error) => {
         console.error('Error:', error);
@@ -28,31 +67,7 @@ export class AppComponent implements AfterViewInit{
         console.log('Request completed');
       }
     });
-    
-    const data = [
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
 
-    new Chart('acquisitions', {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count),
-            backgroundColor: 'rgba(235, 54, 220, 0.75)', // Optional styling
-            
-          }
-        ]
-      }
-    });  
   }
-    
+
 }
