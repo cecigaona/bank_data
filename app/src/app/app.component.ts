7a9e1d8fc25ec80b422dc64f248ac755a9d7cd10
Leonardo Contreras <levt.contreras@proton.me>
2025-05-04 20:01:17 -0600

    Stuff added

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -13,6 +13,10 @@ Chart.register(ChartDataLabels);
 export class AppComponent implements AfterViewInit {
   title = 'app';
 
+  f_size_big = 22
+  f_size_med = 18
+  f_size_small = 14
+
   constructor(private sql: SqlService) {
 
   }
@@ -20,14 +24,14 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit() {
 
     const colorPalette:string[] = [
-      'rgba(110, 215, 220, 0.75)', 
+      'rgba(110, 220, 220, 0.75)', 
       'rgba(110, 200, 220, 0.75)', 
-      'rgba(110, 185, 220, 0.75)',
-      'rgba(110, 170, 220, 0.75)',
-      'rgba(110, 155, 220, 0.75)'
+      'rgba(110, 180, 220, 0.75)',
+      'rgba(110, 160, 220, 0.75)',
+      'rgba(110, 140, 220, 0.75)',
     ]
 
-    // -- CHART 1 --
+    // -- CHART credit_score --
     let query = '\
     SELECT Credit_Score, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM transactions) AS percentage \
     FROM transactions \
@@ -51,7 +55,31 @@ export class AppComponent implements AfterViewInit {
           type: 'pie',
           data: data,
           options: {
+            responsive: true,
+            maintainAspectRatio: false,
             plugins: {
+              title: {
+                display: true,
+                text: 'Credit Score',
+                font: {
+                  size: this.f_size_big
+                },
+                padding: {
+                  top: 10,
+                  bottom: 20
+                }
+              },
+              legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                  font: {
+                    size: this.f_size_small
+                  },
+                  color: '#000',
+                  padding: 20
+                }
+              },
               datalabels: {
                 formatter: (value:number, context:any) => {
                   const dataset = context.chart.data.datasets[0].data;
@@ -65,7 +93,7 @@ export class AppComponent implements AfterViewInit {
                 offset: 30,
                 font: {
                   weight: 'bold',
-                  size: 24,
+                  size: this.f_size_med,
                 }
               }
             }
@@ -83,7 +111,7 @@ export class AppComponent implements AfterViewInit {
     // --------------------
 
 
-    // -- CHART 2 --
+    // -- CHART credit_used --
     query = 'SELECT \
     FLOOR(Credit_Utilization_Ratio / 10) * 10 AS range_start, \
     FLOOR(Credit_Utilization_Ratio / 10) * 10 + 9.99 AS range_end, \
@@ -109,7 +137,31 @@ export class AppComponent implements AfterViewInit {
           type: 'pie',
           data: data,
           options: {
+            responsive: true,
+            maintainAspectRatio: false,
             plugins: {
+              title: {
+                display: true,
+                text: 'Percentage of Credit Used',
+                font: {
+                  size: this.f_size_big
+                },
+                padding: {
+                  top: 10,
+                  bottom: 30
+                }
+              },
+              legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                  font: {
+                    size: this.f_size_small
+                  },
+                  color: '#000',
+                  padding: 20
+                }
+              },
               datalabels: {
                 formatter: (value:number, context:any) => {
                   const dataset = context.chart.data.datasets[0].data;
@@ -123,7 +175,7 @@ export class AppComponent implements AfterViewInit {
                 offset: 30,
                 font: {
                   weight: 'bold',
-                  size: 24,
+                  size: this.f_size_med,
                 }
               }
             }
@@ -141,7 +193,7 @@ export class AppComponent implements AfterViewInit {
 
     // --------------------
 
-    // -- CHART 3 --
+    // -- CHART credit_cards --
     query = 'SELECT \
     FLOOR(Age / 5) * 5 AS age_group_start,\
     FLOOR(Age / 5) * 5 + 4 AS age_group_end,\
@@ -169,7 +221,31 @@ export class AppComponent implements AfterViewInit {
           type: 'bar',
           data: data,
           options: {
+            responsive: true,
+            maintainAspectRatio: false,
             plugins: {
+              title: {
+                display: true,
+                text: 'Credit Card Usage per Age Group',
+                font: {
+                  size: this.f_size_big
+                },
+                padding: {
+                  top: 10,
+                  bottom: 30
+                }
+              },
+              legend: {
+                display: true,
+                position: 'bottom',
+                labels: {
+                  font: {
+                    size: 0
+                  },
+                  color: '#000',
+                  padding: 20
+                }
+              },
               datalabels: {
                 formatter: (value:number, context:any) => {
                   const dataset = context.chart.data.datasets[0].data;
@@ -183,13 +259,182 @@ export class AppComponent implements AfterViewInit {
                 offset: 10,
                 font: {
                   weight: 'bold',
-                  size: 20,
+                  size: this.f_size_med,
                 }
               }
             }
           },
           plugins: [ChartDataLabels]
         });
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+
+    // --------------------
+
+    // -- CHART occupation_interest --
+    query = 'SELECT Occupation, AVG(Interest_Rate) AS Avg_Interest\
+    FROM users\
+    WHERE Interest_Rate IS NOT NULL\
+    GROUP BY Occupation\
+    ORDER BY Avg_Interest DESC\
+    LIMIT 5;';
+
+    this.sql.query(query).subscribe({
+      next: (response) => {
+        console.log('Response data:', response);
+        dataFromResponse = response
+
+        const data = {
+          labels: dataFromResponse.map(row => row.Occupation),
+          datasets: [{
+            data: dataFromResponse.map(row => row.Avg_Interest),
+            backgroundColor: colorPalette,
+          }]
+        };
+
+        new Chart('occupation_interest', {
+          type: 'bar',
+          data: data,
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+              title: {
+                display: true,
+                text: 'Top 5 Occupation with Highest Interest',
+                font: {
+                  size: this.f_size_big
+                },
+                padding: {
+                  top: 10,
+                  bottom: 30
+                }
+              },
+              legend: {
+                display: true,
+                position: 'bottom',
+                labels: {
+                  font: {
+                    size: 0
+                  },
+                  color: '#000',
+                  padding: 20
+                }
+              },
+              datalabels: {
+                formatter: (value:number, context:any) => {
+                  const dataset = context.chart.data.datasets[0].data;
+                  const total = dataset.reduce((a:number, b:number) => a + b, 0);
+                  const percentage = ((value / total) * 100).toFixed(1);
+                  return percentage + '%';
+                },
+                color: '#000',
+                anchor: 'end',
+                align: 'center',
+                offset: 10,
+                font: {
+                  weight: 'bold',
+                  size: this.f_size_med,
+                }
+              }
+            }
+          },
+          plugins: [ChartDataLabels]
+        });
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+
+    // --------------------
+
+    // STAT stat_1
+    // porcentaje de usuarios con deuda no pagada arriba de cierto threshold
+
+    query = 'SELECT \
+    (COUNT(*) * 100.0 / (SELECT COUNT(*) FROM users)) AS perc_users \
+    FROM users  \
+    WHERE Outstanding_Debt > 500;'
+
+    this.sql.query(query).subscribe({
+      next: (response) => {
+        console.log('Response data:', response);
+        dataFromResponse = response
+
+        const statSpan = document.getElementById("stat_1");
+        if (statSpan) {
+          statSpan.textContent = `${(response?.[0]?.perc_users ?? 0).toFixed(1)}%`;
+        }
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+
+    // --------------------
+
+    // STAT stat_2
+    // Promedio en meses del historial crediticio
+
+    query = "SELECT \
+      ROUND(AVG( \
+        (CAST(SUBSTRING_INDEX(Credit_History_Age, ' Years', 1) AS UNSIGNED) * 12) + \
+        (CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(Credit_History_Age, 'and ', -1), ' Month', 1) AS UNSIGNED)) \
+      ), 2) AS Promedio_Meses_Historial \
+    FROM transactions \
+    WHERE Credit_History_Age IS NOT NULL \
+      AND Credit_History_Age LIKE '%Year%' AND Credit_History_Age LIKE '%Month%';"
+
+    this.sql.query(query).subscribe({
+      next: (response) => {
+        console.log('Response data:', response);
+        dataFromResponse = response
+
+        const statSpan = document.getElementById("stat_2");
+        if (statSpan) {
+          statSpan.textContent = `${(response?.[0]?.Promedio_Meses_Historial ?? 0).toFixed(1)}`;
+        }
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+      complete: () => {
+        console.log('Request completed');
+      }
+    });
+
+    // --------------------
+
+    // STAT stat_3
+    // porcentaje de usuarios con deuda no pagada arriba de cierto threshold
+
+    query = 'SELECT \
+    (COUNT(*) * 100.0 / (SELECT COUNT(*) FROM users)) AS perc_users \
+    FROM users  \
+    WHERE Outstanding_Debt > 1500;'
+
+    this.sql.query(query).subscribe({
+      next: (response) => {
+        console.log('Response data:', response);
+        dataFromResponse = response
+
+        const statSpan = document.getElementById("stat_3");
+        if (statSpan) {
+          statSpan.textContent = `${(response?.[0]?.perc_users ?? 0).toFixed(1)}%`;
+        }
       },
       error: (error) => {
         console.error('Error:', error);
